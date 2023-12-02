e1517d0059e890a12bd268bf620f4f68017e3509
Dhruv Bharuka <120679533+dynamicdhxx@users.noreply.github.com>
2023-12-02 13:46:08 +0530

    Update server.py

--- server.py
+++ server.py
@@ -4,18 +4,18 @@ import util
 app = Flask(__name__)
 
 @app.route('/classify_image', methods=['GET', 'POST'])
-def classify_image():
+def predict():
 
-    image_data = request.form['image_data']
+    image_data = request.form['company_name']
     
-    response = jsonify(util.classify_image(image_data))
+    response = jsonify(util.classify_image(stock))
     
     response.headers.add('Access-Control-Allow-Origin', '*')
     
     return response
 
 if __name__ == "__main__":
-    print("Starting Python Flask Server For Sports Celebrity Image Classification")
+    print("Starting Python Flask Server For Stock Prediction")
     util.load_saved_artefacts()
     app.run(port=5000)
 
